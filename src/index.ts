5865fb0d09f41e4092dd73dbe525f786d25d304f
Sergey <redmoloch@yanadex.ru>
2022-05-15 16:20:27 +0300

    added search

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise'; 
 
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const mysql = require('mysql2/promise');
-
-
 
 dotenv.config();
-type DataType = { id: number, name: string }
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -29,6 +23,7 @@ app.get('/', (request, response) => {
         <html>
             <body>
                 <main>
+                <a href="/search">Search</a>
                     <ul>
                     ${data[0].map((item) => {
                         return (
@@ -45,5 +40,69 @@ app.get('/', (request, response) => {
 
         `))
 });
+
+
+// app.get('/search/:queryName', (req, res) => {
+//     const queryName = req.params.queryName;
+//     pool.query(`
+//         SELECT abonents.name, phones.number 
+//         FROM abonents JOIN phones ON abonents.id = phones.abonent_id 
+//         WHERE abonents.name LIKE ?
+//     `, `%${queryName}%`)
+//         // .then((data) => res.json(data))
+//     .then((data: any[]) => res.send(`
+//     <!DOCTYPE html>
+//     <html>
+//         <body>
+//             <main>
+//             <h3>Find : ${data[0].length}</h3>
+//                 <ul>
+
+//                 ${data[0].map((item) => `<li>${item.name} ${item.number}</li>`).join(' ')}
+//                 </ul>
+//             </main>
+//         </body>
+//     </html>
+
+//     `))
+// })
+
+
+app.get('/search', async (req, res) => {
+    const queryName = req.query.name;
+    // console.log(req.query)
+    
+    const data: any[] = await pool.query(`
+        SELECT abonents.name, phones.number 
+        FROM abonents JOIN phones ON abonents.id = phones.abonent_id 
+        WHERE abonents.name LIKE ?
+    `, `%${queryName}%`);
+
+    
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <main>
+            <a href='/'>Main</a>
+            <h3>Find : ${data[0].length}</h3>
+                <ul>
+
+                ${data[0].map((item) => `<li>${item.name} ${item.number}</li>`).join(' ')}
+                </ul>
+                <form method="GET" action="/search">
+                    <label>
+                        Enter Name
+                        <input type='text' name='name' placeholder='name' value="${queryName || ''}"/>
+                        <button type="submit">Search</button>
+                    <label>
+                </form>
+
+            </main>
+        </body>
+    </html>
+
+    `)
+})
 
 app.listen(port, () => console.log(`Running on port ${port}`));
